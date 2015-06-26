e011444812f816d9868cbb8ea3eae73485f8089b
chengvt <vipavee.tri@gmail.com>
2015-06-26 17:38:46 +0900

    add RF-6000 and reformat the names

--- inst/tests/test-readEEM.R
+++ inst/tests/test-readEEM.R
@@ -1,6 +1,7 @@
 context("test readEEM")
 
 test_that("files read are labelled as EEM class", {
-    data <- readEEM(c("rawdata-F7000.txt", "rawdata-FP8500.csv", "rawdata-F7000_Japanese.xls"))
+    data <- readEEM(c("rawdata_F-7000.txt", "rawdata_FP-8500.csv", 
+                      "rawdata_F-7000_Japanese.xls", "rawdata_RF-6000.txt"))
     expect_is(data, "EEM")
 })
