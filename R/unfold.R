d97ab5a8e57bece89a860b31b1303435c2aff01d
chengvt <vipavee.tri@gmail.com>
2016-02-01 13:45:46 +0900

    major edit unfold: make unfold faster by not using melt

--- R/unfold.R
+++ R/unfold.R
@@ -12,46 +12,36 @@
 #' dim(applejuice_uf) # dimension of unfolded matrix
 #' 
 #' @export
-#' @importFrom reshape2 melt
 
-unfold <- function(EEM) UseMethod("unfold")
-
-#' @rdname unfold
-#' @export
-unfold.EEM <-
-  function(EEM){
-    
-    ## check that all EEM has the same dimension
-    dimMat <- sapply(EEM, dim)
-    if (sum(!apply(dimMat, 2, function (x) identical(dimMat[,1], x))) > 0){
-      stop("Dimension do not match. Please check your data.")
+unfold <- function(EEM){
+        
+        ## check that all EEM has the same dimension
+        dimMat <- sapply(EEM, dim)
+        if (sum(!apply(dimMat, 2, function (x) identical(dimMat[,1], x))) > 0){
+            stop("Dimension do not match. Please check your data.")
+        }
+        
+        ## check that EX and EM wavelength are identical for all samples
+        dimension_names <- lapply(EEM, dimnames)
+        if (sum(!sapply(dimension_names, function (x) identical(dimension_names[[1]], x))) > 0){
+            stop("Dimension names do not match. Please check your data.")
+        }
+        
+        # get sName
+        sName <- names(EEM)
+        
+        ## begin unfolding EEM data into a matrix with no. of samples x variables  
+        EEM_uf <- sapply(1:length(EEM), function(i){
+            as.numeric(EEM[[i]])
+        })
+        
+        EEM_uf <- t(EEM_uf)
+        
+        # get var
+        var.full <- expand.grid(em = as.numeric(rownames(EEM[[1]])), ex = as.numeric(colnames(EEM[[1]])))
+        var <- paste0("EX", var.full$ex, "EM", var.full$em)
+        
+        colnames(EEM_uf) <- var
+        rownames(EEM_uf) <- sName
+        return(EEM_uf)
     }
-    
-    ## check that EX and EM wavelength are identical for all samples
-    dimension_names <- lapply(EEM, dimnames)
-    if (sum(!sapply(dimension_names, function (x) identical(dimension_names[[1]], x))) > 0){
-        stop("Dimension names do not match. Please check your data.")
-    }
-
-    # get sName
-    sName <- names(EEM)
-    
-    ## begin unfolding EEM data into a matrix with no. of samples x variables  
-    N <- length(EEM)
-    tmpData <- list()
-    
-    for (i in 1:N){
-      data <- EEM[[i]]
-      meltedData <- melt(as.matrix(data))
-      colnames(meltedData) <- c("Em", "Ex", "intensity")
-      meltedData$var <- paste("EX", meltedData$Ex, "EM", meltedData$Em, sep = "")
-      tmpData[[i]] <- t(meltedData[, 3])
-    }
-    var <- meltedData[, 4]
-    
-    EEM_uf <- do.call(rbind.data.frame, tmpData)
-    EEM_uf <- as.matrix(EEM_uf)
-    colnames(EEM_uf) <- var
-    rownames(EEM_uf) <- sName
-    return(EEM_uf)
-  }
